3b9e7c1f443930715aeebce52d5da5f9181d1a8a
omrigr-code <75146919+omrigr-code@users.noreply.github.com>
2022-05-23 22:09:10 +0300

    omri fixes

---
+++ js/java.js
@@ -0,0 +1,34 @@
+function sendform() {
+    const form = new FormData(document.getElementById('contentForm'));
+    const req = new Request('/PHP/index.php');
+    fetch(req, {
+        method: 'POST',
+        body: form
+    }).then((response)=>{
+        response.text().then((text)=>{
+            alert(text);
+            location.reload();
+        })
+    })
+}
+
+var x = $("#first");
+var y = $("#first1");
+var z = $("#first2");
+x.css("color", "black");
+y.css("color", "black");
+z.css("color", "black");
+ 
+function changeColor() {
+    if (x.css("color") == "rgb(0, 0, 0)") {
+        x.css("color", "red");
+        y.css("color", "red");
+        z.css("color", "red");
+    } else if (x.css("color") == "rgb(255, 0, 0)") {
+        x.css("color", "black");
+        y.css("color", "black");
+        z.css("color", "black");
+    }
+};
+window.setInterval(changeColor, 1000);
+
