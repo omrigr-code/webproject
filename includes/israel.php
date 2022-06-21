3a73bb0abf42c88dd76ff53bba033dfe3ddc1af5
asaf <asafger134@gmail.com>
2022-06-21 21:39:33 +0300

    1

--- includes/israel.php
+++ includes/israel.php
@@ -24,6 +24,7 @@ $recipes = Recipe::GetAllRecipesByCategory($database, "israel");
     <title>The Master's page</title>
     <link rel="stylesheet" href="/css/navbar.css" />
     <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/recipes.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/82bc52e7cb.js" crossorigin="anonymous"></script>
